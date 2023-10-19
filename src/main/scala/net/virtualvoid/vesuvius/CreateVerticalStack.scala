68796966abc822b31a0d88b71f988231b3f3d2e9
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-10-19 15:24:22 +0200

    let's voxel

--- src/main/scala/net/virtualvoid/vesuvius/CreateVerticalStack.scala
+++ src/main/scala/net/virtualvoid/vesuvius/CreateVerticalStack.scala
@@ -1,7 +1,7 @@
 package net.virtualvoid.vesuvius
 
 import java.awt.image.BufferedImage
-import java.io.RandomAccessFile
+import java.io.{ BufferedOutputStream, FileOutputStream, RandomAccessFile }
 import java.nio.ByteOrder
 import java.nio.channels.FileChannel.MapMode
 import javax.imageio.ImageIO
@@ -19,14 +19,14 @@ object Image {
 
     new Image {
       override def getPixel(x: Int, y: Int): Int =
-        buf.get(y * width + x)
+        (buf.get(y * width + x) & 0xffff)
     }
   }
 }
 
 object CreateVerticalStack extends App {
   def layerPath(layer: Int): String =
-    f"/tmp/pap/20230827161847/layers/$layer%02d.tif"
+    f"/home/johannes/tmp/pap/20230827161847/layers/$layer%02d.tif"
 
   val width = 5048
   val height = 9163
@@ -35,28 +35,95 @@ object CreateVerticalStack extends App {
 
   def p(x: Int, y: Int, z: Int): Int = stack(z).getPixel(x, y)
 
-  val xSpan = 3000 to 3500
-  val ySpan = 4400 to 4630
+  val xSpan = 3100 until 3350 //3000 to 3500
+  val ySpan = 4400 until 4630
   val zSpan = 0 to 64
 
-  for {
-    y <- ySpan
+  val x0 = xSpan.head
+  val y0 = ySpan.head
+  val z0 = zSpan.head
 
-  } {
-    val imagePath = f"/tmp/pap/20230827161847/vertical-stack/$y%04d.png"
-    val image = new BufferedImage(xSpan.size, zSpan.size, java.awt.image.BufferedImage.TYPE_INT_RGB)
+  def genSlice(): Unit = {
     for {
-      x <- xSpan
-      z <- zSpan
-    } {
-      val pixel = p(x, y, z)
-      //image.setRGB(z, y, pixel << 8 | pixel)
-      // convert from 16bit gray to 8bit rgb
-      val v = (pixel >> 8) & 0xff
-      image.setRGB(x - xSpan.head, z, v << 16 | v << 8 | v)
-    }
-    require(ImageIO.write(image, "png", new java.io.File(imagePath)))
+      y <- ySpan
 
-    println(s"Wrote $imagePath")
+    } {
+      val imagePath = f"/home/johannes/tmp/pap/20230827161847/vertical-stack/$y%04d.png"
+      val image = new BufferedImage(xSpan.size, zSpan.size, java.awt.image.BufferedImage.TYPE_INT_RGB)
+      for {
+        x <- xSpan
+        z <- zSpan
+      } {
+        val pixel = p(x, y, z)
+        //image.setRGB(z, y, pixel << 8 | pixel)
+        // convert from 16bit gray to 8bit rgb
+        val v = (pixel >> 8) & 0xff
+        image.setRGB(x - xSpan.head, z, v << 16 | v << 8 | v)
+      }
+      require(ImageIO.write(image, "png", new java.io.File(imagePath)))
+
+      println(s"Wrote $imagePath")
+    }
   }
+
+  def threshold(v: Int): Boolean =
+    v > 32000
+
+  val voxels = for {
+    x <- xSpan
+    y <- ySpan
+    z <- zSpan
+    v = p(x, y, z)
+    if threshold(v)
+  } yield (x - x0, y - y0, z - z0, v)
+
+  def genVox(): Unit = {
+    val out = new BufferedOutputStream(new FileOutputStream("out2.vox"))
+
+    def tag(t: String): Unit = {
+      val data = t.getBytes("ASCII")
+      require(data.length == 4)
+      out.write(data)
+    }
+    def uint32le(v: Int): Unit = {
+      out.write(v & 0xff)
+      out.write((v >> 8) & 0xff)
+      out.write((v >> 16) & 0xff)
+      out.write((v >> 24) & 0xff)
+    }
+
+    tag("VOX ")
+    uint32le(200)
+    tag("MAIN")
+    uint32le(0) // MAIN content size
+    uint32le(voxels.size * 4 + 10 * 4 + 259 * 4) // MAIN children size
+
+    tag("SIZE")
+    uint32le(12) // SIZE content size
+    uint32le(0) // SIZE children size
+    uint32le(xSpan.size)
+    uint32le(ySpan.size)
+    uint32le(zSpan.size)
+
+    tag("XYZI")
+    uint32le(4 + 4 * voxels.size) // XYZI content size
+    uint32le(0) // XYZI children size
+    uint32le(voxels.size)
+    for ((x, y, z, v) <- voxels) {
+      val xyzi = x | y << 8 | z << 16 | (v >> 8) << 24
+      uint32le(xyzi)
+    }
+
+    tag("RGBA")
+    uint32le(256 * 4) // RGBA content size
+    uint32le(0) // RGBA children size
+    for (i <- 0 until 256) {
+      val v = 255 - i
+      uint32le(v << 0 | v << 8 | v << 16 | 0xff << 24)
+    }
+
+    out.close()
+  }
+
+  genVox()
 }
