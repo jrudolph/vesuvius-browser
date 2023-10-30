f23f488a2c0998d9e33f8d776d5b6935789cedf7
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-10-30 10:29:26 +0100

    fix scripts

--- worker/run-worker.sh
+++ worker/run-worker.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-curl -JL -o /app.jar $APP_WORK_ENDPOINT/app
-curl -o /deps.jar $APP_WORK_ENDPOINT/deps
+curl --basic -u $APP_DATA_USERNAME:$APP_DATA_PASSWORD -o /app.jar $APP_WORK_ENDPOINT/app
+curl --basic -u $APP_DATA_USERNAME:$APP_DATA_PASSWORD -o /deps.jar $APP_WORK_ENDPOINT/deps
 
 java -cp /app.jar:/deps.jar net.virtualvoid.vesuvius.VesuviusWorkerMain
