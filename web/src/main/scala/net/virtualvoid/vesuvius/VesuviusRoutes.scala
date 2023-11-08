6cf4833428be76e66c44a5b88580d9d81868d41a
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-11-08 17:33:25 +0100

    enable ppm downsampling task for all segments of scroll 1

--- web/src/main/scala/net/virtualvoid/vesuvius/VesuviusRoutes.scala
+++ web/src/main/scala/net/virtualvoid/vesuvius/VesuviusRoutes.scala
@@ -469,7 +469,8 @@ class VesuviusRoutes(config: AppConfig)(implicit system: ActorSystem) extends Di
       InferenceWorkItemInput("youssef-test", 15, 32, true) -> (s => s.scroll == 1 || s.scroll == 2),
       InferenceWorkItemInput("youssef-test", 63, 32, false) -> (s => s.scroll == 332),
       InferenceWorkItemInput("youssef-test", 63, 32, true) -> (s => s.scroll == 332),
-      PPMFingerprintWorkItemInput -> (_.scroll == 1)
+      PPMFingerprintWorkItemInput -> (_.scroll == 1),
+      DownSampleU16_2Input -> (s => s.scroll == 1),
     )
 
   val runnerId = System.currentTimeMillis().toString
