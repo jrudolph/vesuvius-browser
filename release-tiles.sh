d9a78561ca774a07d4ba0d8d99e5d2bcedcad9b3
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-11-10 15:47:15 +0100

    tiles docker setup

---
+++ release-tiles.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+set -x
+
+VERSION=$1
+
+git tag -a tiles-$VERSION -m "Released tiles server $VERSION"
+docker buildx build --builder kube --platform linux/arm64 --tag registry.virtual-void.net/jrudolph/vesuvius-tiles:$VERSION --push -f tiles/Dockerfile .
