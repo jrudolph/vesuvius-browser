81f4685eb0bec24ce56e537ab199a54dbdf314f8
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-07-17 17:46:10 +0200

    tiles: better diagnostics

--- tiles/src/main/scala/net/virtualvoid/vesuvius/tiles/TilesRoutes.scala
+++ tiles/src/main/scala/net/virtualvoid/vesuvius/tiles/TilesRoutes.scala
@@ -399,7 +399,7 @@ class TilesRoutes(config: TilesConfig)(implicit system: ActorSystem) extends Spr
         if (res != 0)
           println(f"madvise failed for $baseAddr%8x $start%8x ${end - start}: $res")
       }
-      println(s"Preloading took ${System.currentTimeMillis() - start}ms")
+      println(f"Preloading of ${maps.size * coalesced.size}%4d took ${System.currentTimeMillis() - start}%5dms")
 
       val startWriting = System.currentTimeMillis()
       //val offs = new scala.collection.immutable.VectorBuilder[Int]()
