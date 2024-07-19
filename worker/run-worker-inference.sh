766aaad72ece849e36cf038bcdff489272ccfa7c
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-07-19 11:14:52 +0200

    revamp worker image to also include grand-prize model
    Signed-off-by: Johannes Rudolph <johannes.rudolph@gmail.com>

---
+++ worker/run-worker-inference.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+cd /model && \
+  curl -OJL https://media.virtual-void.net/s/Pn7CFqPzpJJMJ4G/download/model.ckpt && \
+  curl -OJL https://media.virtual-void.net/s/iXFRiq8wRMHb9X7/download/timesformer_wild15_20230702185753_0_fr_i3depoch=12.ckpt
+
+/run-worker.sh
