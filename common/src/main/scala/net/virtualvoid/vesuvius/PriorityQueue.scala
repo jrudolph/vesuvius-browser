0248a02c8f1a5f53134e3d2549a0d3fcc861afea
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-07-21 19:13:43 +0200

    common: try to fix slow PriorityQueue refresh

--- common/src/main/scala/net/virtualvoid/vesuvius/PriorityQueue.scala
+++ common/src/main/scala/net/virtualvoid/vesuvius/PriorityQueue.scala
@@ -6,51 +6,62 @@ import org.apache.pekko.stream.{ Attributes, Outlet, SourceShape }
 
 import scala.collection.mutable
 
-class PriorityQueueWithRefreshStage[T] extends GraphStageWithMaterializedValue[SourceShape[(T, Long)], PriorityQueue[T]] {
-  val outlet = Outlet[(T, Long)]("PriorityQueueWithRefresh.out")
-  val shape: SourceShape[(T, Long)] = SourceShape(outlet)
+class PriorityQueueWithRefreshStage[K, V] extends GraphStageWithMaterializedValue[SourceShape[(K, V, Long)], PriorityQueue[K, V]] {
+  val outlet = Outlet[(K, V, Long)]("PriorityQueueWithRefresh.out")
+  val shape: SourceShape[(K, V, Long)] = SourceShape(outlet)
 
-  def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, PriorityQueue[T]) = {
-    object Logic extends GraphStageLogic(shape) with PriorityQueue[T] with OutHandler {
+  def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, PriorityQueue[K, V]) = {
+    object Logic extends GraphStageLogic(shape) with PriorityQueue[K, V] with OutHandler {
       setHandler(outlet, this)
 
-      val callback = getAsyncCallback[T] { t =>
-        processOffer(t)
-      }
-      val refreshCallback = getAsyncCallback[T => Boolean] { p =>
-        queue.mapInPlace {
-          case (t, _) if p(t) =>
-            println(s"Reordering $t to front")
-            t -> System.nanoTime()
-          case x => x
+      val callback = getAsyncCallback[(K, V)](processOffer)
+
+      val refreshCallback = getAsyncCallback[K] { key =>
+        val timestamp = System.nanoTime()
+        map.get(key).foreach {
+          case (k, v, oldTimestamp) =>
+            map.put(k, (k, v, timestamp))
+            queue.remove(oldTimestamp)
+            queue.put(timestamp, (k, v))
         }
       }
 
-      val queue: mutable.PriorityQueue[(T, Long)] = mutable.PriorityQueue.empty[(T, Long)](Ordering.by[(T, Long), Long](_._2))
+      val map: mutable.HashMap[K, (K, V, Long)] = mutable.HashMap.empty
+      val queue: mutable.TreeMap[Long, (K, V)] = mutable.TreeMap.empty[Long, (K, V)](Ordering.by(-_))
+      def dequeue(): (K, V, Long) = {
+        val (timestamp, (k, v)) = queue.head
+        queue.remove(timestamp)
+        map.remove(k)
+        (k, v, timestamp)
+      }
 
-      def processOffer(t: T): Unit =
-        if (isAvailable(outlet)) push(outlet, t -> System.nanoTime())
-        else queue.enqueue(t -> System.nanoTime())
+      def processOffer(kv: (K, V)): Unit =
+        if (isAvailable(outlet)) push(outlet, (kv._1, kv._2, System.nanoTime()))
+        else {
+          val timestamp = System.nanoTime()
+          map.put(kv._1, (kv._1, kv._2, timestamp))
+          queue.put(timestamp, kv)
+        }
 
       def onPull(): Unit =
         if (queue.nonEmpty) {
-          push(outlet, queue.dequeue())
+          push(outlet, dequeue())
           if (queue.size % 10 == 0) println(s"Queue size: ${queue.size}")
         }
 
-      def offer(t: T): Unit = callback.invoke(t)
-      def refresh(p: T => Boolean): Unit = refreshCallback.invoke(p)
+      def offer(key: K, value: V): Unit = callback.invoke((key, value))
+      def refresh(key: K): Unit = refreshCallback.invoke(key)
     }
 
     (Logic, Logic)
   }
 }
 
-trait PriorityQueue[T] {
-  def offer(t: T): Unit
-  def refresh(t: T => Boolean): Unit
+trait PriorityQueue[K, V] {
+  def offer(key: K, value: V): Unit
+  def refresh(key: K): Unit
 }
 object PriorityQueue {
-  def queue[T](): Source[(T, Long), PriorityQueue[T]] =
-    Source.fromGraph(new PriorityQueueWithRefreshStage[T])
+  def queue[K, V](): Source[(K, V, Long), PriorityQueue[K, V]] =
+    Source.fromGraph(new PriorityQueueWithRefreshStage[K, V])
 }
