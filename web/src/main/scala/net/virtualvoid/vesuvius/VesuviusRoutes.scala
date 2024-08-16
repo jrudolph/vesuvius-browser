3f8a18a71511d87a50e8918379408af293f4df47
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-08-16 11:45:38 +0200

    browser: include first letters inklabels

--- web/src/main/scala/net/virtualvoid/vesuvius/VesuviusRoutes.scala
+++ web/src/main/scala/net/virtualvoid/vesuvius/VesuviusRoutes.scala
@@ -97,6 +97,7 @@ class VesuviusRoutes(config: AppConfig)(implicit system: ActorSystem) extends Di
   val PolytropeTest3Predictions = externalLayer("polytrope-test3-predictions", "external/polytrope-test3-model")
   val PolytropeInklabels20240816 = externalLayer("polytrope-inklabels-2024-08-16", "external/polytrope-inklabels-2024-08-16")
   val GrandPrizeInklabels = externalLayer("grand-prize-inklabels", "external/grand-prize-inklabels")
+  val FirstLettersInklabels = externalLayer("first-letters-inklabels", "external/first-letters-inklabels")
 
   val InkLabelLayer = LayerDefinition("inklabel", "jpg", AnonymousSource(inklabelFor(_)), isPublic = true)
   val AlphaMaskLayer = LayerDefinition("alpha", "png", AnonymousSource(s => alphaMaskFor(s).map(Some(_))), isPublic = false)
@@ -115,6 +116,7 @@ class VesuviusRoutes(config: AppConfig)(implicit system: ActorSystem) extends Di
       inferenceLayer(Youssef_63_32_ReverseInput, isPublic = true),
       GrandPrizeInklabels,
       PolytropeInklabels20240816,
+      FirstLettersInklabels,
       InkLabelLayer,
       AlphaMaskLayer
     )
