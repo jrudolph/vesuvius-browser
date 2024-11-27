3af91e17c30d6b7bab09f11d9632fac24aeea629
Johannes Rudolph <johannes.rudolph@gmail.com>
2024-11-27 16:12:56 +0100

    tiles: don't use grids for scroll 5 volume for now

--- tiles/src/main/scala/net/virtualvoid/vesuvius/tiles/TilesRoutes.scala
+++ tiles/src/main/scala/net/virtualvoid/vesuvius/tiles/TilesRoutes.scala
@@ -38,7 +38,7 @@ class TilesRoutes(config: TilesConfig)(implicit system: ActorSystem) extends Spr
               },
               path("download" / "64-4") {
                 parameter("x".as[Int], "y".as[Int], "z".as[Int], "bitmask".as[Int], "downsampling".as[Int]) { (x, y, z, bitmask, downsampling) =>
-                  if (scroll.base != FragmentsBase && volume != "20231117161658" && volume != "20230206171837") {
+                  if (scroll.base != FragmentsBase && volume != "20231117161658" && volume != "20230206171837" && volume != "20241024131838") {
                     if (block64x4IsAvailable(scroll, meta, x, y, z, bitmask, downsampling) || gridFileAvailableFor(scroll, meta, x, y, z, downsampling))
                       block64x4(scroll, meta, x, y, z, bitmask, downsampling).deliver
                     else {
